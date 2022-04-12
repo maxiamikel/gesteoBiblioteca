07236ca572680cd13643366e51ae6295f5b95776
maxloversist@gmail.com <maxloversist@gmail.com>
2022-04-12 13:53:19 -0300

    inicializar projeto

---
+++ src/main/java/com/damapi/gestao_biblioteca/GestaoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.damapi.gestao_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damapi/gestao_biblioteca/GestaoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damapi.gestao_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
